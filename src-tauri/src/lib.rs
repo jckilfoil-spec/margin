d480a13c35481aeb6d465411389d1f42b6f00d5c
John Kilfoil <jckilfoil@gmail.com>
2026-05-09 11:23:05 -0400

    feat(updater): Rust plugin registration + capabilities + release CI + RUNBOOK

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             config::get_journal_dir,
             config::set_journal_dir,
